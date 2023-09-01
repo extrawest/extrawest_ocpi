fb4f54a79fec4917feb8cab0a086afcd8ce24c3c
Oleksandr Bozbei <oleksandr.bozbei@extrawest.com>
2023-09-01 16:02:35 +0300

    test: add versions module tests for version 2_1_1

---
+++ tests/test_modules/test_v_2_1_1/test_versions.py
@@ -0,0 +1,80 @@
+from typing import Any
+
+from fastapi.testclient import TestClient
+
+from py_ocpi.main import get_application
+from py_ocpi.core import enums
+from py_ocpi.core.crud import Crud
+from py_ocpi.core.adapter import Adapter
+from py_ocpi.modules.versions.enums import VersionNumber
+from py_ocpi.core.enums import ModuleID, RoleEnum, Action
+
+
+def test_get_versions():
+    app = get_application(
+        [VersionNumber.v_2_1_1], [enums.RoleEnum.cpo], Crud, Adapter
+    )
+
+    client = TestClient(app)
+    response = client.get("/ocpi/versions")
+
+    assert response.status_code == 200
+    assert len(response.json()["data"]) == 1
+
+
+def test_get_versions_v_2_1_1():
+    token = None
+
+    class MockCrud(Crud):
+        @classmethod
+        async def do(
+            cls,
+            module: ModuleID,
+            role: RoleEnum,
+            action: Action,
+            auth_token,
+            *args,
+            data: dict = None,
+            **kwargs,
+        ) -> Any:
+            nonlocal token
+            token = auth_token
+            return {}
+
+    app = get_application(
+        [VersionNumber.v_2_1_1], [enums.RoleEnum.cpo], MockCrud, Adapter
+    )
+
+    client = TestClient(app)
+    response = client.get(
+        "/ocpi/2.1.1/details", headers={"authorization": "Token Zm9v"}
+    )
+
+    assert response.status_code == 200
+    assert response.json()["data"]["version"] == "2.1.1"
+    assert token == "Zm9v"
+
+
+def test_get_versions_v_2_1_1_requires_auth():
+    class MockCrud(Crud):
+        @classmethod
+        async def do(
+            cls,
+            module: ModuleID,
+            role: RoleEnum,
+            action: Action,
+            auth_token,
+            *args,
+            data: dict = None,
+            **kwargs,
+        ) -> Any:
+            return None
+
+    app = get_application(
+        [VersionNumber.v_2_1_1], [enums.RoleEnum.cpo], MockCrud, Adapter
+    )
+
+    client = TestClient(app)
+    response = client.get("/ocpi/2.1.1/details")
+
+    assert response.status_code == 401
