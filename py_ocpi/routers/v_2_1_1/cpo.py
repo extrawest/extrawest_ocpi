7d9734d34b3563ef6004d8272c2406c81e6f3554
Oleksandr Bozbei <oleksandr.bozbei@extrawest.com>
2023-09-26 17:44:29 +0300

    feat: Connect router for tariff module v_2_1_1.

--- py_ocpi/routers/v_2_1_1/cpo.py
+++ py_ocpi/routers/v_2_1_1/cpo.py
@@ -9,10 +9,13 @@ from py_ocpi.modules.locations.v_2_1_1.api import (
 from py_ocpi.modules.cdrs.v_2_1_1.api import (
     cpo_router as cdrs_cpo_2_1_1_router,
 )
-
+from py_ocpi.modules.tariffs.v_2_1_1.api import (
+    cpo_router as tariffs_cpo_2_1_1_router,
+)
 
 router = {
     ModuleID.locations: locations_cpo_2_1_1_router,
     ModuleID.credentials_and_registration: credentials_cpo_2_1_1_router,
     ModuleID.cdrs: cdrs_cpo_2_1_1_router,
+    ModuleID.tariffs: tariffs_cpo_2_1_1_router,
 }
