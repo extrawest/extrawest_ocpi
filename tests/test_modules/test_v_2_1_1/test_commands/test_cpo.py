0e412596ef98f5f456962375b5ae51bdd02823df
Oleksandr Bozbei <oleksandr.bozbei@extrawest.com>
2023-10-20 17:36:08 +0300

    test: Add and update testcases for commands module v_2_1_1.

--- tests/test_modules/test_v_2_1_1/test_commands/test_cpo.py
+++ tests/test_modules/test_v_2_1_1/test_commands/test_cpo.py
@@ -1,3 +1,5 @@
+import pytest
+
 import datetime
 from uuid import uuid4
 
@@ -6,70 +8,59 @@ from fastapi.testclient import TestClient
 from py_ocpi import get_application
 from py_ocpi.core import enums
 from py_ocpi.core.exceptions import NotFoundOCPIError
-from py_ocpi.modules.tokens.v_2_1_1.enums import TokenType, WhitelistType
 from py_ocpi.modules.commands.v_2_1_1.enums import (
     CommandType,
     CommandResponseType,
 )
 from py_ocpi.modules.versions.enums import VersionNumber
 
-from tests.test_modules.test_v_2_1_1.test_tokens import TOKENS
+from .utils import (
+    Crud,
+    ClientAuthenticator,
+    COMMAND_RESPONSE,
+    CPO_BASE_URL,
+    AUTH_HEADERS,
+    WRONG_AUTH_HEADERS,
+)
 
-COMMAND_RESPONSE = {"result": CommandResponseType.accepted}
+from tests.test_modules.test_v_2_1_1.test_tokens.utils import TOKENS
 
-
-class Crud:
-    @classmethod
-    async def do(
-        cls,
-        module: enums.ModuleID,
-        role: enums.RoleEnum,
-        action: enums.Action,
-        *args,
-        data: dict = None,
-        **kwargs,
-    ) -> dict:
-        if action == enums.Action.get_client_token:
-            return "foo"
-
-        return COMMAND_RESPONSE
-
-    @classmethod
-    async def get(
-        cls, module: enums.ModuleID, role: enums.RoleEnum, id, *args, **kwargs
-    ) -> dict:
-        return COMMAND_RESPONSE
-
-    @classmethod
-    async def update(
-        cls,
-        module: enums.ModuleID,
-        role: enums.RoleEnum,
-        data: dict,
-        id,
-        *args,
-        **kwargs,
-    ):
-        ...
+COMMAND_START_URL = f"{CPO_BASE_URL}{CommandType.start_session.value}"
+COMMAND_STOP_URL = f"{CPO_BASE_URL}{CommandType.stop_session.value}"
+RESERVE_NOW_URL = f"{CPO_BASE_URL}{CommandType.reserve_now.value}"
 
 
-def test_cpo_receive_command_start_session_v_2_1_1():
-    app = get_application(
-        version_numbers=[VersionNumber.v_2_1_1],
-        roles=[enums.RoleEnum.cpo],
-        crud=Crud,
-        modules=[enums.ModuleID.commands, enums.ModuleID.sessions],
+@pytest.mark.parametrize(
+    "endpoint",
+    [
+        COMMAND_START_URL,
+        COMMAND_STOP_URL,
+        RESERVE_NOW_URL,
+    ],
+)
+def test_cpo_receive_command_start_session_not_authenticated(
+    client_cpo_v_2_1_1,
+    endpoint,
+):
+    response = client_cpo_v_2_1_1.post(
+        endpoint,
+        headers=WRONG_AUTH_HEADERS,
     )
 
+    assert response.status_code == 403
+
+
+def test_cpo_receive_command_start_session_v_2_1_1(client_cpo_v_2_1_1):
     data = {
         "response_url": "https://dummy.restapiexample.com/api/v1/create",
         "token": TOKENS[0],
         "location_id": str(uuid4()),
     }
 
-    client = TestClient(app)
-    response = client.post(
-        f"/ocpi/cpo/2.1.1/commands/{CommandType.start_session.value}", json=data
+    response = client_cpo_v_2_1_1.post(
+        COMMAND_START_URL,
+        json=data,
+        headers=AUTH_HEADERS,
     )
 
     assert response.status_code == 200
@@ -77,22 +68,16 @@ def test_cpo_receive_command_start_session_v_2_1_1():
     assert response.json()["data"][0]["result"] == COMMAND_RESPONSE["result"]
 
 
-def test_cpo_receive_command_stop_session_v_2_1_1():
-    app = get_application(
-        version_numbers=[VersionNumber.v_2_1_1],
-        roles=[enums.RoleEnum.cpo],
-        crud=Crud,
-        modules=[enums.ModuleID.commands, enums.ModuleID.sessions],
-    )
-
+def test_cpo_receive_command_stop_session_v_2_1_1(client_cpo_v_2_1_1):
     data = {
         "response_url": "https://dummy.restapiexample.com/api/v1/create",
         "session_id": str(uuid4()),
     }
 
-    client = TestClient(app)
-    response = client.post(
-        f"/ocpi/cpo/2.1.1/commands/{CommandType.stop_session.value}", json=data
+    response = client_cpo_v_2_1_1.post(
+        COMMAND_STOP_URL,
+        json=data,
+        headers=AUTH_HEADERS,
     )
 
     assert response.status_code == 200
@@ -100,14 +85,7 @@ def test_cpo_receive_command_stop_session_v_2_1_1():
     assert response.json()["data"][0]["result"] == COMMAND_RESPONSE["result"]
 
 
-def test_cpo_receive_command_reserve_now_v_2_1_1():
-    app = get_application(
-        version_numbers=[VersionNumber.v_2_1_1],
-        roles=[enums.RoleEnum.cpo],
-        crud=Crud,
-        modules=[enums.ModuleID.commands],
-    )
-
+def test_cpo_receive_command_reserve_now_v_2_1_1(client_cpo_v_2_1_1):
     data = {
         "response_url": "https://dummy.restapiexample.com/api/v1/create",
         "token": TOKENS[0],
@@ -118,9 +96,10 @@ def test_cpo_receive_command_reserve_now_v_2_1_1():
         "location_id": str(uuid4()),
     }
 
-    client = TestClient(app)
-    response = client.post(
-        f"/ocpi/cpo/2.1.1/commands/{CommandType.reserve_now.value}", json=data
+    response = client_cpo_v_2_1_1.post(
+        RESERVE_NOW_URL,
+        json=data,
+        headers=AUTH_HEADERS,
     )
 
     assert response.status_code == 200
@@ -145,6 +124,7 @@ def test_cpo_receive_command_reserve_now_unknown_location_v_2_1_1():
         version_numbers=[VersionNumber.v_2_1_1],
         roles=[enums.RoleEnum.cpo],
         crud=Crud,
+        authenticator=ClientAuthenticator,
         modules=[enums.ModuleID.commands],
     )
 
@@ -160,7 +140,9 @@ def test_cpo_receive_command_reserve_now_unknown_location_v_2_1_1():
 
     client = TestClient(app)
     response = client.post(
-        f"/ocpi/cpo/2.1.1/commands/{CommandType.reserve_now.value}", json=data
+        RESERVE_NOW_URL,
+        json=data,
+        headers=AUTH_HEADERS,
     )
 
     assert response.status_code == 200
@@ -168,19 +150,3 @@ def test_cpo_receive_command_reserve_now_unknown_location_v_2_1_1():
 
     # revert Crud changes
     Crud.get = _get
-
-
-def test_emsp_receive_command_result_v_2_1_1():
-    app = get_application(
-        version_numbers=[VersionNumber.v_2_1_1],
-        roles=[enums.RoleEnum.emsp],
-        crud=Crud,
-        modules=[enums.ModuleID.commands],
-    )
-
-    client = TestClient(app)
-    response = client.post(
-        "/ocpi/emsp/2.1.1/commands/1234", json=COMMAND_RESPONSE
-    )
-
-    assert response.status_code == 200
