8cb94642130b882db158c5969a639775c4c79d60
Oleksandr Bozbei <oleksandr.bozbei@extrawest.com>
2023-10-25 17:24:57 +0300

    feat: Add verifiers for push.

--- py_ocpi/core/authentication/verifier.py
+++ py_ocpi/core/authentication/verifier.py
@@ -1,4 +1,4 @@
-from fastapi import Header, Depends
+from fastapi import Header, Depends, Query, Path, WebSocketException, status
 
 from py_ocpi.core.authentication.authenticator import Authenticator
 from py_ocpi.core.dependencies import get_authenticator
@@ -42,3 +42,72 @@ class AuthorizationVerifier:
             await authenticator.authenticate(token)
         except IndexError:
             raise AuthorizationOCPIError
+
+
+class HttpPushVerifier:
+    """
+    A class responsible for verifying authorization tokens if using push.
+    """
+
+    async def __call__(
+        self,
+        authorization: str = Header(...),
+        version: VersionNumber = Path(...),
+        authenticator: Authenticator = Depends(get_authenticator),
+    ):
+        """
+        Verifies the authorization token using the specified version
+        and an Authenticator.
+
+        :param authorization (str): The authorization header containing
+          the token.
+        :param version (VersionNumber): The authorization header containing
+          the token.
+        :param authenticator (Authenticator): An Authenticator instance used
+          for authentication.
+
+        :raises AuthorizationOCPIError: If there is an issue with
+          the authorization token.
+        """
+        try:
+            token = authorization.split()[1]
+            if version.value.startswith("2.2"):
+                token = decode_string_base64(token)
+            await authenticator.authenticate(token)
+        except IndexError:
+            raise AuthorizationOCPIError
+
+
+class WSPushVerifier:
+    """
+    A class responsible for verifying authorization tokens if using ws push.
+    """
+
+    async def __call__(
+        self,
+        token: str = Query(...),
+        version: VersionNumber = Path(...),
+        authenticator: Authenticator = Depends(get_authenticator),
+    ):
+        """
+        Verifies the authorization token using the specified version
+        and an Authenticator.
+
+        :param token (str): Token parameter in ws.
+        :param version (str): The authorization header containing
+          the token.
+        :param authenticator (Authenticator): An Authenticator instance used
+          for authentication.
+
+        :raises AuthorizationOCPIError: If there is an issue with
+          the authorization token.
+        """
+        try:
+            if not token:
+                raise AuthorizationOCPIError
+
+            if version.value.startswith("2.2"):
+                token = decode_string_base64(token)
+            await authenticator.authenticate(token)
+        except AuthorizationOCPIError:
+            raise WebSocketException(code=status.WS_1008_POLICY_VIOLATION)
