aa43bfa344c84ba171b25e99d0f5fd4cb4388eaa
Oleksandr Bozbei <oleksandr.bozbei@extrawest.com>
2023-10-26 16:08:58 +0300

    feat: Add built-in credentials authenticator and verifier.

--- py_ocpi/core/authentication/authenticator.py
+++ py_ocpi/core/authentication/authenticator.py
@@ -20,7 +20,30 @@ class Authenticator(ABC):
             raise AuthorizationOCPIError
 
     @classmethod
+    async def authenticate_credentials(
+        cls,
+        auth_token: str,
+    ) -> str | dict | None:
+        """Authenticate given auth token where both tokens valid."""
+        if auth_token:
+            list_token_a = await cls.get_valid_token_a()
+            if auth_token in list_token_a:
+                return {}
+
+            list_token_c = await cls.get_valid_token_c()
+            if auth_token in list_token_c:
+                return auth_token
+
+        return None
+
+    @classmethod
     @abstractmethod
     async def get_valid_token_c(cls) -> List[str]:
         """Return valid token c list."""
+        pass
+
+    @classmethod
+    @abstractmethod
+    async def get_valid_token_a(cls) -> List[str]:
+        """Return valid token a list."""
         pass
--- py_ocpi/core/authentication/verifier.py
+++ py_ocpi/core/authentication/verifier.py
@@ -44,6 +44,43 @@ class AuthorizationVerifier:
             raise AuthorizationOCPIError
 
 
+class CredentialsAuthorizationVerifier:
+    """
+    A class responsible for verifying authorization tokens
+    based on the specified version number.
+
+    :param version (VersionNumber): OCPI version used.
+    """
+
+    def __init__(self, version: VersionNumber) -> None:
+        self.version = version
+
+    async def __call__(
+        self,
+        authorization: str = Header(...),
+        authenticator: Authenticator = Depends(get_authenticator),
+    ) -> str | dict | None:
+        """
+        Verifies the authorization token using the specified version
+        and an Authenticator.
+
+        :param authorization (str): The authorization header containing
+          the token.
+        :param authenticator (Authenticator): An Authenticator instance used
+          for authentication.
+
+        :raises AuthorizationOCPIError: If there is an issue with
+          the authorization token.
+        """
+        try:
+            token = authorization.split()[1]
+            if self.version.startswith("2.2"):
+                token = decode_string_base64(token)
+            return await authenticator.authenticate_credentials(token)
+        except IndexError:
+            raise AuthorizationOCPIError
+
+
 class HttpPushVerifier:
     """
     A class responsible for verifying authorization tokens if using push.
