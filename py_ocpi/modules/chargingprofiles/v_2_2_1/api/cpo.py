ec723cef3ad9eed122e1d827d2b10cecaef7eed1
Oleksandr Bozbei <oleksandr.bozbei@extrawest.com>
2023-11-15 18:42:16 +0200

    feat: Add delete router

--- py_ocpi/modules/chargingprofiles/v_2_2_1/api/cpo.py
+++ py_ocpi/modules/chargingprofiles/v_2_2_1/api/cpo.py
@@ -13,6 +13,7 @@ from py_ocpi.core.dependencies import get_crud, get_adapter
 
 from py_ocpi.modules.chargingprofiles.v_2_2_1.background_tasks import (
     send_get_chargingprofile,
+    send_delete_chargingprofile,
 )
 from py_ocpi.modules.chargingprofiles.v_2_2_1.schemas import (
     ChargingProfileResponse,
@@ -66,6 +67,75 @@ async def get_chargingprofiles(
                     send_get_chargingprofile,
                     session_id=session_id,
                     duration=duration,
+                    response_url=response_url,
+                    auth_token=auth_token,
+                    crud=crud,
+                    adapter=adapter,
+                )
+            return OCPIResponse(
+                data=[
+                    adapter.charging_profile_response_adapter(
+                        charging_profile_response
+                    ).dict()
+                ],
+                **status.OCPI_1000_GENERIC_SUCESS_CODE,
+            )
+
+        charging_profile_response = ChargingProfileResponse(
+            result=ChargingProfileResponseType.rejected, timeout=0
+        )
+        return OCPIResponse(
+            data=[charging_profile_response.dict()],
+            **status.OCPI_3000_GENERIC_SERVER_ERROR,
+        )
+
+    charging_profile_response = ChargingProfileResponse(
+        result=ChargingProfileResponseType.rejected, timeout=0
+    )
+    return OCPIResponse(
+        data=[charging_profile_response.dict()],
+        **status.OCPI_2000_GENERIC_CLIENT_ERROR,
+    )
+
+
+@router.delete("/{session_id}", response_model=OCPIResponse)
+async def delete_chargingprofiles(
+    request: Request,
+    session_id: CiString(36),  # type: ignore
+    response_url: URL,
+    background_tasks: BackgroundTasks,
+    crud: Crud = Depends(get_crud),
+    adapter: Adapter = Depends(get_adapter),
+):
+    auth_token = get_auth_token(request)
+
+    session = await crud.get(
+        ModuleID.sessions,
+        RoleEnum.cpo,
+        session_id,
+        auth_token=auth_token,
+        version=VersionNumber.v_2_2_1,
+    )
+
+    if session:
+        charging_profile_response = await crud.do(
+            ModuleID.charging_profile,
+            RoleEnum.cpo,
+            Action.send_delete_chargingprofile,
+            session=session,
+            response_url=response_url,
+            auth_token=auth_token,
+            version=VersionNumber.v_2_2_1,
+        )
+
+        if charging_profile_response:
+            if (
+                charging_profile_response["result"]
+                == ChargingProfileResponseType.accepted
+            ):
+                background_tasks.add_task(
+                    send_delete_chargingprofile,
+                    session_id=session_id,
                     response_url=response_url,
                     auth_token=auth_token,
                     crud=crud,
